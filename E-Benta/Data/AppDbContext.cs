5323611ef3601335a2c2c68134d3b9cab804a35d
Marlon Pondavilla <marlon.pondavilla@microsource.com.ph>
2025-12-16 21:04:51 +0800

    Installed entity framework core and tools to prepare set up for database connection

---
+++ E-Benta/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using E_Benta.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Benta.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users => Set<User>();
+    }
+}
--- E-Benta/Program.cs
+++ E-Benta/Program.cs
@@ -1,3 +1,5 @@
+using E_Benta.Data;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+//DB connection string
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<E_Benta.Services.IUserService, E_Benta.Services.UserService>();
 
